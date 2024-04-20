7fa30d38eded80eb99c465edb3cd24a5b80d38fc
Elab <esmael@kali>
2024-04-19 21:21:55 -0500

    new updae

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,8 @@
+#!/usr/bin/puppet
+# Puppet script to install a specific version of Flask (2.1.0) using pip3
+
+# Define the package resource to ensure Flask version 2.1.0 is installed using pip3 provider
 package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3',
+  ensure   => '2.1.0',    # Ensure version 2.1.0 is installed
+  provider => 'pip3',     # Use pip3 as the package provider
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,7 @@
+# Puppet script to kill a process named "killmenow"
+
+# Define the exec resource to run the pkill command to kill the process named "killmenow"
 exec { 'kill_killmenow_process':
-  command => '/usr/bin/pkill -f killmenow',
-  onlyif  => '/usr/bin/pgrep -f killmenow',
+  command  => 'pkill killmenow',  # Command to kill the process
+  provider => 'shell',             # Use shell provider to execute the command
 }
