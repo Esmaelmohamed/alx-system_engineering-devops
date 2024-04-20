2d0ab20e5f76a82705984470bef5c1990dfc809a
Elab <esmael@kali>
2024-04-19 21:20:20 -0500

    update pub

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# Create a file in /tmp with specific permissions, owner, and group
 file { '/tmp/school':
-  ensure   => file,
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
+  ensure  => file,                # Ensure that it's a file
+  content => 'I love Puppet',    # Specify the content of the file
+  mode    => '0744',             # Set file permissions to 0744
+  owner   => 'www-data',         # Set owner to www-data
+  group   => 'www-data',         # Set group to www-data
 }
