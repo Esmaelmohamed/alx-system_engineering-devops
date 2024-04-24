98bc8c53e76b51405d46c03ab27d7d5b6ff64959
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-04-23 17:55:43 -0700

    pubbet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,22 +1,43 @@
-# 100-puppet_ssh_config.pp
+#!/usr/bin/env puppet
 
-# Ensure the SSH client configuration file exists
-file { '/home/your_username/.ssh/config':
-  ensure => present,
-  owner  => 'your_username',
-  group  => 'your_username',
-  mode   => '0600',
+# Puppet script to configure SSH for passwordless authentication.
+
+# Including the standard library module for Puppet
+include stdlib
+
+# Ensure the SSH private key file is specified for authentication.
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]*[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-# Ensure the SSH client is configured to use the private key
-file_line { 'Declare identity file':
-  path  => '/home/your_username/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
+# Regular Expression Explanation:
+# - ^: Matches the beginning of the line
+# - [#]*: Matches zero or more hash characters
+# - [\s]*: Matches zero or more whitespace characters
+# - (?i)IdentityFile: Performs a case-insensitive match for "IdentityFile"
+# - [\s]+: Matches one or more whitespace characters
+# - ~/.ssh/id_rsa: Specifies the path to the SSH private key file to be replaced
+# - $: Matches the end of the line
+
+# Ensure password authentication is disabled.
+file_line { 'Deny Password Authentication':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]*[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-# Ensure the SSH client is configured to refuse password authentication
-file_line { 'Turn off passwd auth':
-  path  => '/home/your_username/.ssh/config',
-  line  => 'PasswordAuthentication no',
-}
+# Regular Expression Explanation:
+# - ^: Matches the beginning of the line
+# - [#]*: Matches zero or more hash characters
+# - [\s]*: Matches zero or more whitespace characters
+# - (?i)PasswordAuthentication: Performs a case-insensitive match for "PasswordAuthentication"
+# - [\s]+: Matches one or more whitespace characters
+# - (yes|no): Matches either "yes" or "no" for password authentication
+# - $: Matches the end of the line
 
