7b532a377053fe80c8bda114c35334daa18c4192
root <root@kali>
2024-06-04 23:47:14 +0300

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""Module to fetch the number of subscribers of a subreddit"""
+"""Module for retrieving the number of subscribers of a subreddit"""
 
 import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API to retrieve the number of subscribers for a given subreddit.
-    
+    Queries the Reddit API to get the number of subscribers for a subreddit.
+
     Args:
         subreddit (str): The name of the subreddit.
-    
+
     Returns:
         int: The number of subscribers, or 0 if the subreddit is invalid or an error occurs.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "custom-user-agent-for-subreddit-query"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            return 0
-
-        data = response.json().get("data", {})
-        return data.get("subscribers", 0)
-    except requests.RequestException:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code >= 300:
         return 0
+
+    return response.json().get("data", {}).get("subscribers", 0)
 
