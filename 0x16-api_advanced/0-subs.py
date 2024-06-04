b09dbb2c9aa440a982024d0cc0dc689a931453de
Elab <esmael@kali>
2024-06-04 21:51:06 +0300

    sys

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'python:subreddit.subscriber.count:v1.0 (by /u/yourusername)'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception as e:
+        return 0
+
