9dae2817ab7de14019b319dc296e6ee886b4c0d2
root <root@kali>
2024-06-04 23:57:16 +0300

    0 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""Module for retrieving the number of subscribers of a subreddit"""
-
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
 import requests
+
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API to get the number of subscribers for a subreddit.
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
 
     Args:
         subreddit (str): The name of the subreddit.
 
     Returns:
-        int: The number of subscribers, or 0 if the subreddit is invalid or an error occurs.
+        int: The number of total subscribers for the subreddit.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
+    url = f"http://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code >= 300:
+    if response.status_code != 200:
         return 0
-
-    return response.json().get("data", {}).get("subscribers", 0)
+    
+    subscriber_count = response.json().get('data', {}).get('subscribers', 0)
+    return subscriber_count
 
