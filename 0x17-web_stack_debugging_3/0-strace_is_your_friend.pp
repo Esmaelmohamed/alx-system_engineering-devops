c72d2a5d11ef702c261e4af36b5c552e41f12d98
Elab <esmael@kali>
2024-06-06 02:24:26 +0300

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,13 @@
-exec { 'fix_typo':
-  command => 'mv /var/www/html/wp-includes/class-wp-locale.phpp /var/www/html/wp-includes/class-wp-locale.php',
-  path    => '/bin/',
+# 0-strace_is_your_friend.pp
+file { '/var/www/html/wp-includes/class-wp-locale.php':
+  ensure  => file,
+  content => 'class WP_Locale {}', # Placeholder content, adjust as necessary
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
 }
 
+service { 'apache2':
+  ensure     => 'running',
+  enable     => true,
+  subscribe  => File['/var/www/html/wp-includes/class-wp-locale.php'],
